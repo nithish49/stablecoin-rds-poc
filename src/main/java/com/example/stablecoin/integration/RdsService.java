131cd7b8f2db53687ce6d49e27b3668b7dad6299
nithish <yadavallinithishkumar@gmail.com>
2026-01-21 08:01:21 +0530

    Updated the Code with Settlement Logic

--- src/main/java/com/example/stablecoin/integration/RdsService.java
+++
@@ -1,7 +0,0 @@
-package com.example.stablecoin.integration;
-import com.example.stablecoin.model.CardTransaction;
-import org.springframework.stereotype.Service;
-@Service
-public class RdsService {
-    public boolean requiresStablecoin(CardTransaction tx){ return true; }
-}
